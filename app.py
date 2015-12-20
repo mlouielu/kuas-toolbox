e5c81b624c764af361fa10f3284a34b94f628ac3
Louie Lu <git@louie.lu>
2015-12-20 12:08:44 +0800

    update captial

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import meeting
 import const
 import requests
 from lxml import etree
@@ -79,6 +80,32 @@ def committee():
                            ga=const.ga_string)
 
 
+@app.route("/ppp")
+def ppp():
+    p = {
+        "name": "會計系", "link": "https://www.facebook.com/acckuas",
+        "name": "觀光管理系", "link": "https://www.facebook.com/KUASTMA",
+        "name": "土木工程系", "link": "https://www.facebook.com/KUASCivilEngineering",
+        "name": "財富與稅務管理學系", "link": "https://www.facebook.com/KUAS103rdWTM",
+        "name": "企業管理系", "link": "https://www.facebook.com/kuasba",
+        "name": "資訊管理系", "link": "https://www.facebook.com/KUASMISSA",
+        "name": "資訊工程系", "link": "https://www.facebook.com/kuascsie",
+        "name": "應用外語系", "link": "https://www.facebook.com/KUASxAFLD",
+        "name": "文化創意產業學系", "link": "https://www.facebook.com/I.LIKE.CCI",
+        "name": "模具工程系", "link": "https://www.facebook.com/國立高雄應用科技大學-模具工程系學會-446635782136841",
+        "name": "電子工程系學會", "link": "https://www.facebook.com/國立高雄應用科技大學-電子工程系學會-200392033367356",
+        "name": "金融系", "link": "https://www.facebook.com/KUAS.FIN",
+        "name": "工業與工程管理系", "link": "https://www.facebook.com/kuasiem",
+        "name": "機械工程系", "link": "https://www.facebook.com/kuasme",
+        "name": "電機工程系", "link": "https://www.facebook.com/國立高雄應用科技大學-電機工程系學會-483608054998190",
+        "name": "國際企業系", "link": "https://www.facebook.com/kuasib",
+        "name": "人力資源發展系", "link": "https://www.facebook.com/HRD102nd",
+        "name": "化學工程與材料工程系", "link": "https://www.facebook.com/103CME",
+    }
+
+    return ""
+
+
 @app.route("/bameeting")
 def bameeting():
     uam, acm = get_meeting_list()
@@ -86,6 +113,11 @@ def bameeting():
     return render_template("bameeting.html",
                            uam=uam,
                            acm=acm)
+
+
+@app.route("/bameeting_detail/<string:issue_no>")
+def bameeting_detail_issue(issue_no):
+    root = meeting.get_meeting_root(issue_no)
 
 
 @app.route("/bameeting/<string:issue_no>")
